e6327602b6902711a522ff909c2b26a6e4e65dfc
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-06-25 15:42:12 +0530

    Update HeroHeader component content and styles

--- src/app/components/HeroHeader/hero-header.component.tsx
+++ src/app/components/HeroHeader/hero-header.component.tsx
@@ -2,8 +2,13 @@ import styles from './hero-header.module.scss'
 
 export default function HeroHeader() {
   return (
-    <div className={styles.mainContainer}>
-      
+    <div className={`${styles.mainContainer} w-full text-center`}>
+      <div className={`${styles.heroTitle} font-extrabold`}></div>
+      <div className={`${styles.heroSubTitle}`}>Full-Stack Developer</div>
+      <div className={`${styles.contacts} flex justify-center`}>
+          <div className={`${styles.location}`}>India</div>
+          <div className={`${styles.gmail}`}></div>
+      </div>
     </div>
   );
 }
