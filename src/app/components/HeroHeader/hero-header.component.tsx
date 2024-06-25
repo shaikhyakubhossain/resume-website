ec5293b3d342482aceeb6c85e36c87ec50ba55a7
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-06-25 20:57:09 +0530

    Update HeroHeader component content and styles

--- src/app/components/HeroHeader/hero-header.component.tsx
+++ src/app/components/HeroHeader/hero-header.component.tsx
@@ -1,13 +1,15 @@
-import styles from './hero-header.module.scss'
+import styles from './hero-header.module.scss';
+import { HiMapPin } from "react-icons/hi2";
+import { HiMail } from "react-icons/hi";
 
 export default function HeroHeader() {
   return (
     <div className={`${styles.mainContainer} w-full text-center`}>
-      <div className={`${styles.heroTitle} font-extrabold`}></div>
+      <div className={`${styles.heroTitle} font-extrabold`}>Shaikh Yakub Hossain</div>
       <div className={`${styles.heroSubTitle}`}>Full-Stack Developer</div>
       <div className={`${styles.contacts} flex justify-center`}>
-          <div className={`${styles.location}`}>India</div>
-          <div className={`${styles.gmail}`}></div>
+          <div className={`${styles.locationContainer}`}><HiMapPin className={`${styles.HiMapPin} my-auto mr-1`} /><div className={`${styles.location}`}>India</div></div>
+          <div className={`${styles.gmailContainer}`}><HiMail className={`${styles.HiMail} my-auto mr-1`} /><div className={`${styles.gmail}`}>shaikhyakubhossain@gmail.com</div></div>
       </div>
     </div>
   );
