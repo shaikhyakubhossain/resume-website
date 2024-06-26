dac3667cb942ad9a4f3114b55afc40e035b5e434
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-06-26 17:38:59 +0530

    Update LinkBtn component

--- src/app/components/LinkBtn/link-btn.component.tsx
+++ src/app/components/LinkBtn/link-btn.component.tsx
@@ -1,20 +1,20 @@
 import { IconType } from "react-icons";
 
 type propsType = {
-    customBtnClassName?: string,
-    customIconClassName?: string,
-    customTextClassName?: string,
-    customTWStyle: string,
-    children: React.ReactNode,
-    icon: IconType,
-    txtBorderIconColor: string
+  icon: IconType,
+  children: React.ReactNode,
+  txtBorderIconColor: string
+  customTWStyle: string,
+  // customBtnClassName?: string,
+  // customIconClassName?: string,
+  // customTextClassName?: string,
 }
 
 export default function LinkBtn(props: propsType) {
   return (
     <button style={{borderColor: props.txtBorderIconColor}} className={`flex mx-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border-2 hover:border-transparent rounded-full` + props.customTWStyle}>
-        <div className={` my-auto mr-1 text-2xl`}>{<props.icon />}</div>
-        <div className={``} >{props.children}</div>
+        <div style={{color: props.txtBorderIconColor}} className={` my-auto mr-1 text-2xl`}>{<props.icon />}</div>
+        <div style={{color: props.txtBorderIconColor}} className={``} >{props.children}</div>
     </button>
   );
 }
