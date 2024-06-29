7cebd2d0cfe27029c9b34059dca8dbcde0f8884e
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-06-29 12:04:10 +0530

    Update SectionDetail component

--- src/app/components/SectionDetail/section-detail.component.tsx
+++ src/app/components/SectionDetail/section-detail.component.tsx
@@ -2,49 +2,58 @@ import styles from "./section-detail.module.scss";
 import { HiOutlineCalendarDays } from "react-icons/hi2";
 
 type propsType = {
-    title: string,
-    subTitle: string,
-    dateFrom: string,
-    dateTo: string,
-    techStack?: string[],
-    details?: string[]
-
-}
+  title: string;
+  subTitle: string;
+  dateFrom: string;
+  dateTo: string;
+  techStack?: string[];
+  details?: string[];
+};
 
 export default function SectionDetail(props: propsType): JSX.Element {
   return (
     <div className={styles.mainContainer}>
-      <div className={`${styles.header} flex`}>
-        <div className={`${styles.title}`}></div>
-        <div> — </div>
-        <div className={`${styles.subTitle}`}></div>
+      <div className={`${styles.header} flex justify-between mr-32`}>
+        <div className={`${styles.titleAndSubTitleContainer} flex`}>
+          <div className={`${styles.title} font-bold`}>{props.title}</div>
+          <div>&nbsp;—&nbsp;</div>
+          <div className={`${styles.subTitle}`}>{props.subTitle}</div>
+        </div>
+
         <div className={`${styles.date} flex`}>
-            <div className={`${styles.calendarIcon}`}><HiOutlineCalendarDays /></div>
-            <div className={`${styles.dateFrom}`}></div>
-            <div className={``}> - </div>
-            <div className={`${styles.dateTo}`}></div>
+          <div className={`${styles.calendarIcon} my-auto`}>
+            <HiOutlineCalendarDays />
+          </div>
+          <div className={`${styles.dateFrom}`}>{props.dateFrom}</div>
+          <div className={``}>&nbsp;-&nbsp; </div>
+          <div className={`${styles.dateTo}`}>{props.dateTo}</div>
         </div>
       </div>
       <div className={`${styles.techStack}`}>
-        {
-            props.techStack ? props.techStack.map((item, index) => {
-                return (
-                    <span key={index} className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">{item}</span>
-                )
-            }) : null
-        }
+        {props.techStack
+          ? props.techStack.map((item, index) => {
+              return (
+                <span
+                  key={index}
+                  className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300"
+                >
+                  {item}
+                </span>
+              );
+            })
+          : null}
       </div>
       <div className={`${styles.details}`}>
-            {
-                props.details ? props.details.map((item, index) => {
-                    return (
-                        <div>
-                            <div></div>
-                            <div></div>
-                        </div>
-                    )
-                }) : null
-            }
+        {props.details
+          ? props.details.map((item, index) => {
+              return (
+                <div>
+                  <div></div>
+                  <div></div>
+                </div>
+              );
+            })
+          : null}
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import "./globals.css"
 import HeroHeader from "./components/HeroHeader/hero-header.component";
 import Section from "./components/Section/section.component";
+import SectionDetail from "./components/SectionDetail/section-detail.component";
 
 export default function Home() {
   return (
     <div>
       <HeroHeader />
-      <Section sectionName="Education">hello</Section>
+      <Section sectionName="Education"><SectionDetail title={"test"} subTitle={"testing"} dateFrom="june 29" dateTo="present" /></Section>
     </div>
   );
 }
